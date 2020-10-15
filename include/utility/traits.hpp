01cfff0ba7fce143bff8d53196bbc677d2527707
sablinkv <k.v.sablin@gmail.com>
2020-10-15 20:25:26 +0500

    update traits

--- include/utility/traits.hpp
+++ include/utility/traits.hpp
@@ -14,28 +14,39 @@ namespace algorithms {
 	template<class Iterator>
 	using iterator_difference_t = typename std::iterator_traits<Iterator>::difference_type;
 
+	template <class Iterator, class = void>
+	inline constexpr bool is_iterator_v = false;
+
+	template <class Iterator>
+	inline constexpr bool is_iterator_v<Iterator, std::void_t<iterator_category_t<Iterator>>> = true;
+
 	template<class Iterator>
-	struct is_const_iterator
-	{
+	using enable_if_iterator = std::enable_if_t<is_iterator_v<Iterator>>;
+
+	template<class Iterator>
+	using disable_if_iterator = std::enable_if_t<!is_iterator_v<Iterator>>;
+	
+	template<class Iterator>
+	struct is_const_iterator {
 		using pointer = typename std::iterator_traits<Iterator>::pointer;
-		static constexpr bool value = std::is_const<typename std::remove_pointer<pointer>::type>::value;
+		static constexpr bool value = std::is_const_v<typename std::remove_pointer<pointer>::type>;
 	};
 
 	template<class Iterator>
-	constexpr bool is_const_iterator_v = is_const_iterator<Iterator>::value;
+	inline constexpr bool is_const_iterator_v = is_const_iterator<Iterator>::value;
 
-	template<class Iterator, class Iterator_tag>
+	template<class Current_iterator, class Required_iterator>
 	struct is_sortable {
-		static constexpr bool value = std::is_convertible_v<iterator_category_t<Iterator>, Iterator_tag>
-			&& !is_const_iterator_v<Iterator>;
+		static constexpr bool value = std::is_convertible_v<iterator_category_t<Current_iterator>, Required_iterator>
+			&& !is_const_iterator_v<Current_iterator>;
 	};
 
-	template<class Iterator, class Iterator_tag>
-	constexpr bool is_sortable_v = is_sortable<Iterator, Iterator_tag>::value;
+	template<class Current_iterator, class Required_iterator>
+	inline constexpr bool is_sortable_v = is_sortable<Current_iterator, Required_iterator>::value;
 
 	template<class Sorter>
 	struct is_stable : std::false_type {};
 
 	template<class Sorter>
-	constexpr bool is_stable_v = is_stable<Sorter>::value;
+	inline constexpr bool is_stable_v = is_stable<Sorter>::value;
 } // namespace algorithms
